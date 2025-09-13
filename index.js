ca54547947719e9b256738bc8079a1e56e2c10a1
shafee_azeem <shafeeghani@gmail.com>
2025-09-13 21:59:18 +0530

    brain dead

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 function factorial(n) {
-    if (n === 0) return 1;
+    if (n === 0) return 0
+    else if (n === 1) return 1
 
-    let result = 1;
+    let first_number = 0;
+    let second_number = 1;
 
-    for (let i = n; i >= 1; i--) {
-        result *= i;
+    for (let i = 2; i <= n; i++) {
+        next_number = first_number + second_number;
+        first_number = second_number;
+        second_number = next_number;
     }
 
-    return result;
+    return second_number;
 
 }
 
-console.log(factorial(3));
-console.log(factorial(5));
+console.log(factorial(0));
+console.log(factorial(1));
 console.log(factorial(2));
+console.log(factorial(3));
 console.log(factorial(4));
+console.log(factorial(5));
+console.log(factorial(6));
